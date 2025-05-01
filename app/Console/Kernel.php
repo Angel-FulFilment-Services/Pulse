658099dee6fc10398b85fa9f334d1995f0350d7a
Luke Shakespear <luke@angelfs.com>
2025-05-01 09:35:52 +0100

    Enabled automated scheduled task.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:BuildAbsenceEventsCommand')->everyMinute();
+        $schedule->command('command:BuildAbsenceEventsCommand')->dailyAt('6:00');
     }
 
     /**
