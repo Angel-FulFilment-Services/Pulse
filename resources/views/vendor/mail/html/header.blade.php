d26ccabf1d735fea998a60ed5aac80c226cf2430
Luke Shakespear <luke@angelfs.com>
2025-08-26 20:14:07 +0100

    Fix to image not embedding into email.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,16 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) == 'Pulse - Angel Fulfilment Services')
-<img src="data:image/webp;base64,{{ base64_encode(file_get_contents(public_path('images/hero-large.webp'))) }}" height="75" class="logo" alt="Pulse">
+@php
+    $imagePath = public_path('images/hero-large.webp');
+    $imageExists = file_exists($imagePath);
+    $imageData = $imageExists ? base64_encode(file_get_contents($imagePath)) : '';
+@endphp
+@if($imageExists && $imageData)
+<img src="data:image/webp;base64,{{ $imageData }}" height="75" class="logo" alt="Pulse" style="display: block;">
+@else
+<span style="font-size: 18px; font-weight: bold; color: #333;">Pulse - Angel Fulfilment Services</span>
+@endif
 @else
 {{ $slot }}
 @endif
--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,6 +2,7 @@
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
+{{ config('app.name') }}
 </x-mail::header>
 </x-slot:header>
 
