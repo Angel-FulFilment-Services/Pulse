7385060934f1d6903753fd43feb254e89f80b5ff
Luke Shakespear <luke@angelfs.com>
2025-08-26 20:28:01 +0100

    Removed conditional logic in the email header.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,13 +2,9 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) == 'Pulse - Angel Fulfilment Services' || strpos($slot, 'pulse.angelfs.co.uk') !== false)
 <div style="text-align: center; width: 100%;">
 <img src="{{ asset('images/hero-large.webp') }}" height="75" class="logo" alt="Pulse" style="display: block; margin: 0 auto; max-width: 100%;">
 </div>
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
