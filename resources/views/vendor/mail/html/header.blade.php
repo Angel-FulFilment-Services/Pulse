a189ddd663716a5b54ea760fd1253c969e85f18d
Luke Shakespear <luke@angelfs.com>
2025-08-26 20:22:26 +0100

    Minor fix to center align email image.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,10 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) == 'Pulse - Angel Fulfilment Services')
-<img src="{{ asset('images/hero-large.webp') }}" height="75" class="logo" alt="Pulse" style="display: block; max-width: 100%;">
+@if (trim($slot) == 'Pulse - Angel Fulfilment Services' || strpos($slot, 'pulse.angelfs.co.uk') !== false)
+<div style="text-align: center; width: 100%;">
+<img src="{{ asset('images/hero-large.webp') }}" height="75" class="logo" alt="Pulse" style="display: block; margin: 0 auto; max-width: 100%;">
+</div>
 @else
 {{ $slot }}
 @endif
