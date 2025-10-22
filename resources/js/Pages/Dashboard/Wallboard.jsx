b7ae7f89973e49b2dca2903059d5b4cb38c1bdc5
Luke Shakespear <luke@angelfs.com>
2025-10-22 16:22:13 +0100

    Minor adjustment to wallboard.

--- resources/js/Pages/Dashboard/Wallboard.jsx
+++ resources/js/Pages/Dashboard/Wallboard.jsx
@@ -4,7 +4,7 @@ const Wallboard = () => {
     return (
         <div className="flex h-screen w-screen overflow-hidden bg-gray-900">
             {/* Left iframe - 65% width */}
-            <div className="w-[65%] h-full">
+            <div className="w-[70%] h-full">
                 <iframe 
                     src="https://wings.angelfs.co.uk"
                     className="w-full h-full border-0"
@@ -13,7 +13,7 @@ const Wallboard = () => {
             </div>
             
             {/* Right iframe - 35% width */}
-            <div className="w-[35%] h-full">
+            <div className="w-[30%] h-full">
                 <iframe 
                     src="https://pulse.angelfs.co.uk/onsite/widgets/access-control"
                     className="w-full h-full border-0"
