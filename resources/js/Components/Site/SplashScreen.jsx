04e329c3f2ed6df176dcecbf99ba55138d309e91
Luke Shakespear <luke@angelfs.com>
2025-10-30 13:21:47 +0000

    Changes to site sign in function.

--- resources/js/Components/Site/SplashScreen.jsx
+++ resources/js/Components/Site/SplashScreen.jsx
@@ -15,20 +15,25 @@ export default function SplashScreen({ onContinue, setStep }) {
         <div className="h-full w-full flex flex-col items-center justify-start px-10 z-50"> 
           <div className="flex h-2/5 items-start justify-start">
             <img
-              src="/images/angel-logo.png"
+              src="/images/angel-logo-tag-line.png"
               alt="Logo"
-              className="w-72"
+              className="h-52"
             />
           </div>
-          <div className="flex flex-col items-start justify-start -mt-8">
-            <h1 className="text-9xl font-semibold text-white dark:text-dark-100 mb-6">
-              Hello,
+          <div className="flex flex-col items-start justify-start -mt-14">
+            <h1 className="text-9xl font-semibold text-white dark:text-dark-100 -ml-2">
+              Welcome to
+              <img
+                src="/images/angel-logo-light.png"
+                alt="Logo"
+                className="h-52 inline-block ml-6 mb-3"
+              />
             </h1>
-            <h1 className="text-7xl font-semibold text-white dark:text-dark-100 mb-6">
-              Please sign in here.
+            <h1 className="text-5xl font-semibold text-white dark:text-dark-100 mb-6 -mt-6 max-w-[54rem]">
+              If you’re visiting us today, please sign in here so we know you’ve arrived, then take a seat.
             </h1>
-            <h1 className="text-4xl font-semibold text-white/85 dark:text-dark-100 mb-6 ml-1">
-              For Angel employees and visitors only.
+            <h1 className="text-4xl font-semibold text-white dark:text-dark-100 mb-6 ml-1">
+              We’ll be with you shortly.
             </h1>
           </div>
         </div>
@@ -49,7 +54,7 @@ export default function SplashScreen({ onContinue, setStep }) {
       </div>
       <div
         className="z-50 p-6 absolute right-10 top-1/2 -mt-16 text-center bg-[rgba(0,141,169,1)] text-white rounded-3xl cursor-pointer text-xl shadow-[0_0_35px_0_rgba(0,0,0,0.1)] font-bold focus:outline-none ring-1 ring-black ring-opacity-5 flex flex-col items-center"
-        onClick={(event) => { console.log('Test'); setStep('delivery-type-home'); event.stopPropagation(); }}
+        onClick={(event) => { setStep('delivery-type-home'); event.stopPropagation(); }}
       >
         <TruckIcon className="h-14 w-14 inline-block text-white" />
       </div>
